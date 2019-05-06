4279c208ca5cfc3bd664797358d2967fa52ebd37
Anton Repin <anton.repin@alation.com>
2019-05-05 17:56:29 -0700

    Abstract coding task updated to carry more print outputs

--- src/main/java/examples/AbstractCodingTask.java
+++ src/main/java/examples/AbstractCodingTask.java
@@ -5,17 +5,27 @@ import java.util.Arrays;
 public class AbstractCodingTask {
 
     public void printResultForInput(String sessionId, int[] input, int result) {
-        System.out.println("Session id " +sessionId+ ". Input:");
+        System.out.println("\nSession id " +sessionId+ ". Input:");
         System.out.println(Arrays.toString(input));
         System.out.println("Result: ");
         System.out.println(result);
     }
 
     public void printResultForInput(String sessionId, int[] input, int[] results) {
-        System.out.println("Session id " +sessionId+ ". Input:");
+        System.out.println("\nSession id " +sessionId+ ". Input:");
         System.out.println(Arrays.toString(input));
         System.out.println("Result: ");
         System.out.println(Arrays.toString(results));
+    }
+
+    public void printResultForInput(String sessionId, String input, boolean result) {
+        System.out.println("\nSession id " +sessionId+ ". Input:");
+        System.out.println(input);
+        System.out.println("Result: \n" + result);
+    }
+
+    public void printTaskHeader() {
+        System.out.println("----------------TASK---------------\n");
     }
 
 }
